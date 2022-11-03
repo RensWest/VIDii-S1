5abab8cf757021bd30da573992726b3c32918fff
RensWest <94118091+RensWest@users.noreply.github.com>
2022-11-03 10:42:46 +0100

    poez

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,14 @@
 // JavaScript Document
 console.log("Howdy!");
+
+const mushroomButton = document.getElementById('mushroomBtn');
+const mushroomImg = document.getElementById('mushroomImg')
+
+mushroomButton.addEventListener("click", playAnimation);
+
+function playAnimation() {
+    // code to be executed
+    console.log('test2');
+    mushroomImg.classList.add('mushroomAnimation');
+
+  }
