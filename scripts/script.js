66ec7d0e42ef474f4303c4095476d08ccea2fcb8
RensWest <94118091+RensWest@users.noreply.github.com>
2022-11-04 12:39:10 +0100

    YEAHHH

--- scripts/script.js
+++ scripts/script.js
@@ -2,30 +2,69 @@
 
 // Buttons Shell en Shroom
 var mushroomButton = document.querySelector('#mushroomBtn');
-var mushroomImg = document.querySelector('#mushroomImg');
 var shellButton = document.querySelector('#shellBtn');
-var shellImg = document.querySelector('#shellImg');
 
 mushroomButton.addEventListener("click", playAnimationShroom);
+var counter = 1;
+
 
     // classlist add en remove na 3000ms
 function playAnimationShroom() {
+    mushroomButton.disabled = true; // Button disabelen zodat er niet kan worden geklikt zolang animatie speelt
+    var mushroomImg = document.querySelector('#mushroomImg'); 
     mushroomImg.classList.add('mushroomAnimation');
     setTimeout(() => {
         mushroomImg.classList.remove('mushroomAnimation');
-      }, 3000)
+        mushroomButton.disabled = false; // button weer aan
+      }, 3000); // delay van de classlist remove
+
+      var fotoMario = document.querySelector('main > section:first-of-type li img:nth-of-type(' + counter + ')');
+      setTimeout(() => {
+        fotoMario.classList.add("vergroot");
+        setTimeout(() => {
+            fotoMario.classList.remove("vergroot");
+        }, 3000); // delay van de classlist remove
+
+        // Jeffrey heeft mij hiermee geholpen
+        if(counter < 11){
+            setTimeout(() =>{
+
+            
+            var contentSelector = document.querySelector('ol li:nth-child(' + counter + ')') // update queryselector met counter
+            var marioSelector = document.querySelector('main > section:first-of-type li img:nth-child(' + counter + ')')
+            
+            contentSelector.classList.add('hidden')
+            marioSelector.classList.add('hidden')
+            
+            counter++
+            
+            contentSelector = document.querySelector('ol li:nth-child(' + counter + ')') // update queryselector met counter
+            marioSelector = document.querySelector('main > section:first-of-type li img:nth-child(' + counter + ')') 
+            
+            contentSelector.classList.remove('hidden')
+            marioSelector.classList.remove('hidden')
+            },1020)
+          } 
+      }, 2000) // delay van de functie en classlist add
+      
+      
   };
 
 shellButton.addEventListener("click", playAnimationShell);
 
 function playAnimationShell() {
+    var shellImg = document.querySelector('#shellImg');
     shellImg.classList.add('shellAnimation');
     setTimeout(() => {
         shellImg.classList.remove('shellAnimation');
-    }, 3000)
-};
+    }, 3000);
 
-// foto/titel/tekst aanpassen op groter/kleiner worden
-        // var hierFotoMario = document.querySelector("#mariofoto");
-        // var hierTitelMario = document.querySelector("ol li h2");
-        // var hierTekstMario = document.querySelector("ol li p");
+    var fotoMario = document.querySelector("main > section:first-of-type li img");
+      setTimeout(() => {
+        fotoMario.classList.add("verklein");
+        setTimeout(() => {
+            fotoMario.classList.remove("verklein");
+        }, 3000);
+        // delay van de animatie
+      }, 2000)
+};
