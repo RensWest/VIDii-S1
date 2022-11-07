6242001a72a40a2762dcf24de62c43f9f6e029af
RensWest <94118091+RensWest@users.noreply.github.com>
2022-11-07 12:26:20 +0100

    afasdgas

--- scripts/script.js
+++ scripts/script.js
@@ -52,61 +52,84 @@ function playAnimationShroom() {
 
 shellButton.addEventListener("click", playAnimationShell);
 
+
+// function playAnimationShell() {
+//     var shellImg = document.querySelector('#shellImg'); 
+
+//     var fotoMario = document.querySelector('main > section:first-of-type li img:nth-of-type(' + counter + ')');
+//     if(counter == 1){
+//         // shellButton.disabled = true; // Button disabelen zodat er niet kan worden geklikt zolang animatie speelt
+//         console.log('Je kan niet verder terug :D')
+//     }else{
+//         shellImg.classList.add('shellAnimation');
+//         setTimeout(() => {
+//             shellImg.classList.remove('shellIAnimation');
+//             shellButton.disabled = false; // button weer aan
+//           }, 3000); // delay van de classlist remove
+//           setTimeout(() => {
+//             fotoMario.classList.add("verklein");
+//             setTimeout(() => {
+//                 fotoMario.classList.remove("verklein");
+//             }, 3000); // delay van de classlist remove
+    
+//             // Jeffrey heeft mij hiermee geholpen
+//                 setTimeout(() =>{
+    
+                
+//                 var contentSelector = document.querySelector('ol li:nth-child(' + counter + ')') // update queryselector met counter
+//                 var marioSelector = document.querySelector('main > section:first-of-type li img:nth-child(' + counter + ')')
+                
+//                 contentSelector.classList.add('hidden')
+//                 marioSelector.classList.add('hidden')
+                
+//                 counter--
+//                 contentSelector = document.querySelector('ol li:nth-child(' + counter + ')') // update queryselector met counter
+//                 marioSelector = document.querySelector('main > section:first-of-type li img:nth-child(' + counter + ')') 
+                
+//                 contentSelector.classList.remove('hidden')
+//                 marioSelector.classList.remove('hidden')
+//                 },2820)
+//           }, 2000) // delay van de functie en classlist add
+//     }
+        
+//     };
+
+
 function playAnimationShell() {
-    var shellImg = document.querySelector('#shellImg');
-    shellImg.classList.add('shellAnimation');
-    setTimeout(() => {
-        shellImg.classList.remove('shellAnimation');
-    }, 3000);
-
-    var fotoMario = document.querySelector("main > section:first-of-type li img");
-      setTimeout(() => {
-        fotoMario.classList.add("verklein");
-        setTimeout(() => {
-            fotoMario.classList.remove("verklein");
-        }, 3000);
-        // delay van de animatie
-      }, 2000)
-};
-
-
-function playAnimationShell() {
-    shellButton.disabled = true; // Button disabelen zodat er niet kan worden geklikt zolang animatie speelt
     var shellImg = document.querySelector('#shellImg'); 
-    shellImg.classList.add('shellAnimation');
-    setTimeout(() => {
-        shellImg.classList.remove('shellIAnimation');
-        shellButton.disabled = false; // button weer aan
-      }, 3000); // delay van de classlist remove
 
-      var fotoMario = document.querySelector('main > section:first-of-type li img:nth-of-type(' + counter + ')');
-      setTimeout(() => {
-        fotoMario.classList.add("verklein");
+    var fotoMario = document.querySelector('main > section:first-of-type li img:nth-of-type(' + counter + ')');
+    if(counter > 1){
+        shellButton.disabled = true; // Button disabelen zodat er niet kan worden geklikt zolang animatie speelt
+        console.log('Je kan niet verder terug :D')
+        shellImg.classList.add('shellAnimation');
         setTimeout(() => {
-            fotoMario.classList.remove("verklein");
-        }, 3000); // delay van de classlist remove
-
-        // Jeffrey heeft mij hiermee geholpen
-        if(counter < 11){
-            setTimeout(() =>{
-
-            
-            var contentSelector = document.querySelector('ol li:nth-child(' + counter + ')') // update queryselector met counter
-            var marioSelector = document.querySelector('main > section:first-of-type li img:nth-child(' + counter + ')')
-            
-            contentSelector.classList.add('hidden')
-            marioSelector.classList.add('hidden')
-            
-            counter--
-            
-            contentSelector = document.querySelector('ol li:nth-child(' + counter + ')') // update queryselector met counter
-            marioSelector = document.querySelector('main > section:first-of-type li img:nth-child(' + counter + ')') 
-            
-            contentSelector.classList.remove('hidden')
-            marioSelector.classList.remove('hidden')
-            },2820)
-          } 
-      }, 2000) // delay van de functie en classlist add
-      
-      
-  };
+            shellImg.classList.remove('shellAnimation');
+            shellButton.disabled = false; // button weer aan
+          }, 3000); // delay van de classlist remove
+          setTimeout(() => {
+            fotoMario.classList.add("verklein");
+            setTimeout(() => {
+                fotoMario.classList.remove("verklein");
+            }, 3000); // delay van de classlist remove
+    
+            // Jeffrey heeft mij hiermee geholpen
+                setTimeout(() =>{
+    
+                
+                var contentSelector = document.querySelector('ol li:nth-child(' + counter + ')') // update queryselector met counter
+                var marioSelector = document.querySelector('main > section:first-of-type li img:nth-child(' + counter + ')')
+                
+                contentSelector.classList.add('hidden')
+                marioSelector.classList.add('hidden')
+                
+                counter--
+                contentSelector = document.querySelector('ol li:nth-child(' + counter + ')') // update queryselector met counter
+                marioSelector = document.querySelector('main > section:first-of-type li img:nth-child(' + counter + ')') 
+                
+                contentSelector.classList.remove('hidden')
+                marioSelector.classList.remove('hidden')
+                },2820)
+          }, 2000) // delay van de functie en classlist add
+    };
+}
